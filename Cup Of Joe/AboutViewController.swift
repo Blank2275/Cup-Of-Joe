018a32838595894aa5d87b944a78b750de107082
Connor <cfr3.14156@gmail.com>
2021-11-09 10:27:53 -0700

    Fixed about page and major changes

--- Cup Of Joe/AboutViewController.swift
+++ Cup Of Joe/AboutViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
     }
     
 
